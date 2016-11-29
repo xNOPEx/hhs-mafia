30c67d8f41f81ff09e4a3ecf10f31da591bd908b
xNOPEx <aidan.samwick@gmail.com>
2016-11-29 11:00:56 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Tofu Dad home page">
+		<meta name="keywords" content="">
+		<link rel="icon" href="/content/photos/favicon.ico" type="image/x-icon" />
+		<link rel="shortcut icon" href="/content/photos/favicon.ico" type="image/x-icon" />
+		<title>Tofu Dad</title>
+		<link rel="stylesheet" href="css/base.css">
+	</head>
+  
+	<body>
+		<header>
+			
+		</header>
+	
+		<nav>
+			
+		</nav>
+
+		<main>
+			<p>yo</p>
+		</main>
+
+		<footer>
+			
+		</footer>
+	</body>
+</html>
